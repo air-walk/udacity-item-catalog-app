b6909320602634b55c4f37d6b1c406152d0f8145
Air Walk <air-walk@users.noreply.github.com>
2017-07-19 20:04:45 -0400

    defining the models

---
+++ vagrant/catalog/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+
+Base = declarative_base()
+
+class Category(Base):
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+
+class Item(Base):
+    __tablename__ = 'item'
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    title = Column(String(250), nullable=False)
+    cat_id = Column(Integer, ForeignKey('category.id'))
+    cat = relationship(Category)
+
+
+engine = create_engine('sqlite:///catalog.db')
+
+
+Base.metadata.create_all(engine)
