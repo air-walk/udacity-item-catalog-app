35d4134b18a0cb32468034b6f528c52285396b39
Air Walk <air-walk@users.noreply.github.com>
2017-07-19 20:40:06 -0400

    adding serializeable property to models

--- vagrant/catalog/models.py
+++ vagrant/catalog/models.py
@@ -11,6 +11,15 @@ class Category(Base):
     id = Column(Integer, primary_key = True)
     name = Column(String(80), nullable = False)
 
+    @property
+    def serialize(self):
+      """Return category data in a serializeable format"""
+      return {
+          'id':   self.id,
+          'name': self.name
+      }
+
+
 class Item(Base):
     __tablename__ = 'item'
     id = Column(Integer, primary_key=True)
@@ -18,6 +27,16 @@ class Item(Base):
     title = Column(String(250), nullable=False)
     cat_id = Column(Integer, ForeignKey('category.id'))
     cat = relationship(Category)
+
+    @property
+    def serialize(self):
+      """Return item data in a serializeable format"""
+      return {
+          'id':          self.id,
+          'description': self.description,
+          'title':       self.title,
+          'cat_id':      self.cat_id
+      }
 
 
 engine = create_engine('sqlite:///catalog.db')
