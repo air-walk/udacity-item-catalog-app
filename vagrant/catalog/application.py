2d9d6e9e399b6f0176152f6de30d3d7bcb0f9050
Air Walk <air-walk@users.noreply.github.com>
2017-07-22 18:20:47 -0400

    adding the capability to sign-in via Google+

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
+from flask import session as login_session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base, Category, Item
+import random, json, string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
 
 
 app = Flask(__name__)
@@ -10,14 +15,72 @@ engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
-session   = DBSession()
+db_session   = DBSession()
+
+
+@app.route('/login')
+def login():
+  """Shows login page to a user to sign in via Google sign in."""
+  state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+  login_session['state'] = state
+  return render_template('login.html', STATE=state)
+
+
+@app.route('/gconnect', methods=['GET', 'POST'])
+def gconnect():
+  """Responsible for getting authorization from user, and then accessing their Google data using Google APIs.
+  This is the auth endpoint, which handles response from the OAuth2.0 server."""
+  # Create the 'Flow' object based on client's secrets and scope
+  flow = flow_from_clientsecrets('client_secrets.json',
+                                  scope='https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+                                  redirect_uri='http://localhost:8000/gconnect')
+  flow.params['access_type']            = 'offline'
+  flow.params['include_granted_scopes'] = 'true'        # For incremental authZ
+
+  if request.method == 'POST':
+    # Get the authorization URL and redirect the user to it, so that they can grant (or deny) application's access to their Google data
+    auth_uri = flow.step1_get_authorize_url()
+    print "[STEP 1]: Sending user to Google to authenticate him/herself and provide consent to the application.."
+
+    return redirect(auth_uri)
+  else:
+    print "[STEP 2]: Received authZ consent/decline from user.."
+    code = request.args.get('code')
+    credentials = flow.step2_exchange(code)
+
+    # Create an HTTP object and apply credentials to it so that all requests using this instance are authorized
+    http = httplib2.Http()
+    http = credentials.authorize(http)
+
+    # Send a request to Google+ API to get user info
+    print "[STEP 3]: Attempting to retrieve user info from Google+.."
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo?alt=json"
+    response     = http.request(userinfo_url)
+
+    # If request succeeded, store the user's info in session and redirect to home page else route the user back to login page
+    if response[0].status == 200:
+      user_info = json.loads(response[1])
+
+      # print user_info["email"]
+      # print user_info["name"]
+      # print user_info["picture"]
+
+      login_session["email"]   = user_info["email"]
+      login_session["name"]    = user_info["name"]
+      login_session["picture"] = user_info["picture"]
+      print "User identified as %s, redirecting to home page..." %(login_session["email"])
+
+      return redirect("/")
+    else:
+      print "[" + response[0].status + " ERROR]: Something went wrong!"
+      return redirect(url_for("login"))
 
 
 @app.route('/')
 @app.route('/categories')
 def showCategories():
   """Base route. Shows all categories and latest items."""
-  categories = session.query(Category).all()
+  categories = db_session.query(Category).all()
   return render_template('categories.html', categories = categories)
 
 
@@ -27,8 +90,8 @@ def newCategory():
   """Creates a new category."""
   if request.method == 'POST':
     newCategory = Category(name = request.form['name'])
-    session.add(newCategory)
-    session.commit()
+    db_session.add(newCategory)
+    db_session.commit()
 
     flash("Category creation successful!")
     return redirect(url_for('showCategories'))
@@ -40,11 +103,11 @@ def newCategory():
 @app.route('/categories/<int:category_id>/edit/', methods=['GET', 'POST'])
 def editCategory(category_id):
   """Edits a category."""
-  category = session.query(Category).filter_by(id = category_id).one()
+  category = db_session.query(Category).filter_by(id = category_id).one()
 
   if request.method == 'POST':
     category.name = request.form['name']
-    session.commit()
+    db_session.commit()
 
     flash("Category edit successful!")
     return redirect(url_for('showCategories'))
@@ -56,17 +119,17 @@ def editCategory(category_id):
 @app.route('/categories/<int:category_id>/delete/', methods=['GET', 'POST'])
 def deleteCategory(category_id):
   """Deletes a category."""
-  category = session.query(Category).filter_by(id = category_id).one()
+  category = db_session.query(Category).filter_by(id = category_id).one()
 
   if request.method == 'POST':
-    session.delete(category)
+    db_session.delete(category)
 
     # Once a category is deleted, delete related items as well
-    items = session.query(Item).filter_by(cat_id = category_id).all()
+    items = db_session.query(Item).filter_by(cat_id = category_id).all()
     for item in items:
-      session.delete(item)
+      db_session.delete(item)
 
-    session.commit()
+    db_session.commit()
 
     flash("Category (and related items) deletion successful!")
     return redirect(url_for('showCategories'))
@@ -80,8 +143,8 @@ def deleteCategory(category_id):
 @app.route('/categories/<int:category_id>/')
 def showItems(category_id):
   """Shows all items for a catalog entry."""
-  category = session.query(Category).filter_by(id = category_id).one()
-  items    = session.query(Item).filter_by(cat_id = category_id).all()
+  category = db_session.query(Category).filter_by(id = category_id).one()
+  items    = db_session.query(Item).filter_by(cat_id = category_id).all()
 
   return render_template('items.html', category = category, items = items)
 
@@ -90,7 +153,7 @@ def showItems(category_id):
 @app.route('/categories/<int:category_id>/<int:item_id>')
 def showItem(category_id, item_id):
   """Shows a particular item from a catalog entry."""
-  item = session.query(Item).filter_by(id = item_id, cat_id = category_id).one()
+  item = db_session.query(Item).filter_by(id = item_id, cat_id = category_id).one()
 
   return render_template('item.html', item = item)
 
@@ -99,12 +162,12 @@ def showItem(category_id, item_id):
 @app.route('/categories/<int:category_id>/new', methods=['GET', 'POST'])
 def newItem(category_id):
   """Creates an item in catalog."""
-  category = session.query(Category).filter_by(id = category_id).one()
+  category = db_session.query(Category).filter_by(id = category_id).one()
 
   if request.method == 'POST':
     newItem = Item(cat_id = category_id, title = request.form['title'], description = request.form['description'])
-    session.add(newItem)
-    session.commit()
+    db_session.add(newItem)
+    db_session.commit()
 
     flash("Item creation successful!")
     return redirect(url_for('showItems', category_id = category_id))
@@ -116,13 +179,13 @@ def newItem(category_id):
 @app.route('/categories/<int:category_id>/<int:item_id>/edit', methods=['GET', 'POST'])
 def editItem(category_id, item_id):
   """Edits a particular item from a catalog entry."""
-  category = session.query(Category).filter_by(id = category_id).one()
-  item     = session.query(Item).filter_by(id = item_id, cat_id = category_id).one()
+  category = db_session.query(Category).filter_by(id = category_id).one()
+  item     = db_session.query(Item).filter_by(id = item_id, cat_id = category_id).one()
 
   if request.method == 'POST':
     item.title       = request.form['title']
     item.description = request.form['description']
-    session.commit()
+    db_session.commit()
 
     flash("Item edit successful!")
     return redirect(url_for('showItems', category_id = category_id))
@@ -134,12 +197,12 @@ def editItem(category_id, item_id):
 @app.route('/categories/<int:category_id>/<int:item_id>/delete', methods=['GET', 'POST'])
 def deleteItem(category_id, item_id):
   """Deletes a particular item from a catalog entry."""
-  category = session.query(Category).filter_by(id = category_id).one()
-  item     = session.query(Item).filter_by(id = item_id, cat_id = category_id).one()
+  category = db_session.query(Category).filter_by(id = category_id).one()
+  item     = db_session.query(Item).filter_by(id = item_id, cat_id = category_id).one()
 
   if request.method == 'POST':
-    session.delete(item)
-    session.commit()
+    db_session.delete(item)
+    db_session.commit()
 
     flash("Item deletion successful!")
     return redirect(url_for('showItems', category_id = category_id))
@@ -151,7 +214,7 @@ def deleteItem(category_id, item_id):
 @app.route('/categories.json')
 def showCatalogJSON():
   """Shows catalog in JSON format"""
-  categories = session.query(Category).all()
+  categories = db_session.query(Category).all()
   return jsonify(category = [category.serialize for category in categories])
 
 
@@ -159,7 +222,7 @@ def showCatalogJSON():
 @app.route('/categories/<int:category_id>.json')
 def showItemsJSON(category_id):
   """Shows items in JSON format"""
-  items = session.query(Item).filter_by(cat_id = category_id)
+  items = db_session.query(Item).filter_by(cat_id = category_id)
   return jsonify(item = [item.serialize for item in items])
 
 
